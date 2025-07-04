97969dcf7394f499b8841ed8ee80c4e8eea1efd6
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-07-05 00:12:14 +0530

    modified:   regexai/core/templates/core/layout.html 	new file:   regexai/core/templates/core/robots.txt 	modified:   regexai/core/urls.py

--- regexai/core/urls.py
+++ regexai/core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+from django.views.generic import TemplateView
+path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"))
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("generate/", views.generate_regex, name="generate"),
